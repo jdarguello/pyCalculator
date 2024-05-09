de19fa223944f0a57745eaeb530b2ecc26932dea
Juan David Arguello Plata <jdarguel@bancolombia.com.co>
2024-05-09 11:46:43 -0500

    suma

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.Calculadora import Calculadora
+
+if __name__ == "__main__":
+    cal = Calculadora()
+    print("1+2 =",cal.suma(1,2))
--- src/Calculadora.py
+++ src/Calculadora.py
@@ -1,9 +1,9 @@
 class Calculadora:
-    def suma(self, numeros):
+    def suma(self, *numeros):
         valor = 0
         for numero in numeros:
             valor += numero
         return valor
 
-    def resta(self, numeros):
+    def resta(self, *numeros):
         pass
